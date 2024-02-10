a0d3f67f9dab463141bcd4957d57acb6b40f5ace
EdAlv02 <edsonali14@gmail.com>
2024-02-09 23:57:43 -0600

    Featured: Add a new Header

--- js/function.js
+++ js/function.js
@@ -55,4 +55,13 @@ $(document).ready(function() {
     });
   });
 
-  
+  $(function(){
+    $(window).scroll(function(){
+      var winTop = $(window).scrollTop();
+      if(winTop >= 30){
+        $("body").addClass("sticky-header");
+      }else{
+        $("body").removeClass("sticky-header");
+      }//if-else
+    });//win func.
+  });//ready func.
