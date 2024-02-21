ba4d0c35f094695a0fb3e59de53d7495dd34af57
EdAlv02 <edsonali14@gmail.com>
2024-02-21 00:54:04 -0600

    Feature: New header on index.html |

--- js/template.js
+++ js/template.js
@@ -14,50 +14,14 @@ function pageName() {
   console.log(part);
 
 }
+const nav = document.getElementById('navbar');
 
+const navbar = document.querySelector('.navbar');
 
-// const pageLoc = window.location.href;
-
-// function getPageName() {
-//   const url = window.location.href;
-//   const parts = url.split("/");
-//   return parts[parts.length - 1];
-// }
-// const pageName = getPageName();
-// console.log(pageName);
-
-// var inicio = document.getElementById('inicioNavLink');
-
-// switch (pageName) {
-//   case "index.html":
-//     inicio.style.borderBottom = "1px solid #EDDC5A";
-//     inicio.style.borderRadius = "3px";
-//     console.log("Si sirve");
-//     break;
-//   case "nosotros.html":
-//     console.log("Si sirve");
-//     inicio.style.borderBottom = "1px solid #EDDC5A";
-//     inicio.style.borderRadius = "3px";
-//     break;
-//   case "servicios.html":
-//     console.log("Si sirve");
-//     link.classList.add("inicio-style");
-//     for (const link of inicio) {
-//     }
-//     break;
-//   case "portafolio.html":
-//     console.log("Si sirve");
-//     for (const link of inicio) {
-//       link.classList.add("inicio-style");
-//     }
-//     break;
-//   case "contacto.html":
-//     console.log("Si sirve");
-//     for (const link of inicio) {
-//       link.classList.add("inicio-style");
-//     }
-//     break;
-//   default:
-//     console.log("No sirve");
-// }
-
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 0) {
+        navbar.classList.add('scrolled');
+      } else {
+        navbar.classList.remove('scrolled');
+      }
+    });
