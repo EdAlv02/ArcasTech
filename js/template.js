597a3205144212597592bba1ec272ca1c5491086
Alan050102 <alan.nuno4804@alumnos.udg.mx>
2024-02-21 20:52:45 -0600

    Fix: Deletes arrow.js and navbar.js, to add it to template.js

--- js/template.js
+++ js/template.js
@@ -14,4 +14,26 @@ function pageName() {
   console.log(part);
 
 }
-   
+//Funcion navbar cambio de color
+window.addEventListener('scroll', function() {
+  var header = document.querySelector('header');
+  var navbarContainer = document.querySelector('.navbar');
+
+  if (window.scrollY > header.clientHeight) {
+    navbarContainer.classList.add('scrolled');
+  } else {
+    navbarContainer.classList.remove('scrolled');
+  }
+});
+
+//Funcion de back-top para hacerlo visible
+window.addEventListener('scroll', function() {
+  var backtop = document.getElementById('back-top');
+  if (window.scrollY > 50) {
+    backtop.classList.add('visible');
+    backtop.classList.remove('invisible');
+  } else {
+    backtop.classList.remove('visible');
+    backtop.classList.add('invisible');
+  }
+}); 
